7d8a4494e34d409766c7d51a14de703f4cb78df5
baelanche <baelanche@naver.com>
2019-12-07 19:51:09 +0900

    Log In, user edit

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controller/videoController";
-import { getJoin, postJoin, login, logout } from "../controller/userController";
+import { getJoin, postJoin, logout, getLogin, postLogin } from "../controller/userController";
 
 const globalRouter = express.Router();
 
@@ -10,7 +10,9 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { videos, videoDetail, editVideo, deleteVideo, upload } from "../controller/videoController";
+import { videoDetail, editVideo, deleteVideo, upload } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
