2d71450d74498ac02cb180cf1dfb4993fc630925
baelanche <baelanche@naver.com>
2019-12-10 21:15:36 +0900

    add show/upload video

--- db.js
+++ db.js
@@ -1,38 +1,20 @@
-export const videos = [
-    {
-        id: 324,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 123,
-            name: 'Baegyu',
-            email: 'baelanche@naver.com'
-        }
-    },
-    {
-        id: 325,
-        title: 'Video awesome22',
-        description: 'This is something I like',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 123,
-            name: 'Baegyu',
-            email: 'baelanche@naver.com'
-        }
-    },
-    {
-        id: 326,
-        title: 'Video awesome333',
-        description: 'This is something I hate',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 123,
-            name: 'Baegyu',
-            email: 'baelanche@naver.com'
-        }
-    }
-]
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//search mongod
+mongoose.connect(
+    process.env.MONGO_URL, 
+{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
