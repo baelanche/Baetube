5e2fcf143b100c9b4f8507ffd483e64b4fedc020
baelanche <baelanche@naver.com>
2019-12-16 18:58:01 +0900

    add user authenticate

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,16 +1,23 @@
 import express from "express";
 import routes from "../routes";
-import { videoDetail, getEditVideo, deleteVideo, getUpload, postUpload, postEditVideo } from "../controller/videoController";
-import { uploadVideo } from "../middleware";
+import {
+  videoDetail,
+  getEditVideo,
+  deleteVideo,
+  getUpload,
+  postUpload,
+  postEditVideo
+} from "../controller/videoController";
+import { uploadVideo, onlyPrivate } from "../middleware";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
